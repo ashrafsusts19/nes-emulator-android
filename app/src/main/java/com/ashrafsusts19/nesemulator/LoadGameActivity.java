36ff973493145a62c11cd56ae0afc69b81cee51a
ashrafsusts19 <ashrafsusts19@gmail.com>
2023-03-09 00:07:36 +0600

    Setup Permissions for Storage Access
    Build functionalities to allow app to request storage access. Created functions for each option pressed in the main menu. Will be completed latter
    
    Revert "Setup Permissions for Storage Access"
    
    This reverts commit bd4892c8c1ccfb30fe4b7f5c12c38401aae2a8e9.
    
    Revert "Revert "Setup Permissions for Storage Access""
    
    This reverts commit 2001811c7bb0e3ad96b5468a869fc50480b8fe9b.

---
+++ app/src/main/java/com/ashrafsusts19/nesemulator/LoadGameActivity.java
@@ -0,0 +1,14 @@
+package com.ashrafsusts19.nesemulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_game);
+    }
+}
--- app/src/main/java/com/ashrafsusts19/nesemulator/MainActivityHome.java
+++ app/src/main/java/com/ashrafsusts19/nesemulator/MainActivityHome.java
@@ -1,7 +1,14 @@
 package com.ashrafsusts19.nesemulator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,6 +22,7 @@ import java.util.Arrays;
 public class MainActivityHome extends AppCompatActivity {
 
     private ListView optionsList;
+    private int STORAGE_PERMISSION_CODE = 1;
     ArrayList<String> options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +42,101 @@ public class MainActivityHome extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivityHome.this, options.get(i), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivityHome.this, options.get(i), Toast.LENGTH_SHORT).show();
+                switch(options.get(i)){
+                    case "Load Rom":
+                        loadRomOptionPressed();
+                        break;
+                    case "Start":
+                        startOptionPressed();
+                        break;
+                    case "Recent Games":
+                        recentGamesOptionPressed();
+                        break;
+                    case "Options":
+                        optionsOptionPressed();
+                        break;
+                    case "About":
+                        aboutOptionPressed();
+                        break;
+                    case "Exit":
+                        exitOptionPressed();
+                        break;
+                }
             }
         });
 
+    }
+
+    private void loadRomOptionPressed(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED){
+            requestStoragePermission();
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            // TODO: load loadGameActivity
+
+        }
+    }
+
+    private void startOptionPressed() {
+
+    }
+
+    private void recentGamesOptionPressed() {
+
+    }
+
+    private void optionsOptionPressed() {
+
+    }
+
+    private void aboutOptionPressed() {
+
+    }
+
+    private void exitOptionPressed() {
+
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("Permission Needed to Access Storage")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivityHome.this, new String[]
+                                    {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    }).show();
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
